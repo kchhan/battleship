72e1d1bb91186fdce2c717e1732a0d0a9f5874a1
Kevin Chhan <kvnchhan@gmail.com>
2020-08-26 11:33:08 -0700

    add @babel/polyfill as dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,6 @@
 const path = require('path');
 
-var fs = require('fs');
-
-var nodeModules = {};
-fs.readdirSync('node_modules')
-  .filter(function (x) {
-    return ['.bin'].indexOf(x) === -1;
-  })
-  .forEach(function (mod) {
-    nodeModules[mod] = 'commonjs ' + mod;
-  });
-
 module.exports = {
-  mode: 'development',
-  externals: nodeModules,
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -23,15 +10,18 @@ module.exports = {
     rules: [
       {
         test: /\.js?$/,
-        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-modules-amd'],
           },
         },
       },
     ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    port: 8080,
   },
 };
